68594fe224b810ceb4085235551677ffe90cb29a
0x7E <codeberg.unmapped@0xaa.me>
2025-06-20 04:19:46 +0200

    Update flake.lock Update hosts/hikari/nixos/media/prowlarr.nix Update nixos/core/secrets/protonvpn.nix

--- hosts/hikari/nixos/media/prowlarr.nix
+++ hosts/hikari/nixos/media/prowlarr.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, ... }:
+{ pkgs, ... }:
 
 {
   services.prowlarr = {
@@ -8,18 +8,16 @@
   };
 
   virtualisation.oci-containers.containers.flaresolverr = {
-    image = "flaresolverr/flaresolverr:v3.3.21";
+    image = "21hsmw/flaresolverr:nodriver";
 
     imageFile = pkgs.dockerTools.pullImage {
-      imageName = "flaresolverr/flaresolverr";
-      imageDigest = "sha256:f104ee51e5124d83cf3be9b37480649355d223f7d8f9e453d0d5ef06c6e3b31b";
-      sha256 = "sha256-N5NY89albL7Kws9pBRoDtW3Ae2vIKH1k1+9nVQJ3ltU=";
+      imageName = "21hsmw/flaresolverr";
+      imageDigest = "sha256:dca8cda5852b04e6142752fc044c9845eb536353ea6f8b7bb58b4ff9419538fa";
+      sha256 = "sha256-Cifd9O+3Gi/ThzlQqbwJyK6FQYx5fGiFm/stJ0n1zQo=";
 
-      finalImageTag = "v3.3.21";
+      finalImageTag = "nodriver";
     };
 
     ports = [ "8191:8191" ];
-
-    environment.TZ = config.time.timeZone;
   };
 }
--- nixos/core/secrets/protonvpn.nix
+++ nixos/core/secrets/protonvpn.nix
@@ -6,40 +6,70 @@
   ...
 }:
 
+let
+  gw-ip = "10.2.0.1";
+in
 lib.mkIf config.me.secrets.enable {
   sops.secrets."proton_vpn/endpoint" = { };
   sops.secrets."proton_vpn/pk" = { };
+  sops.secrets."proton_vpn/sk" = { };
 
   sops.templates."proton-vpn.conf".content =
     let
       inherit (myLib) network;
       writeScriptFile = name: text: ((pkgs.writeShellScriptBin name text) + "/bin/${name}");
-      postUpFile = writeScriptFile "postUp.sh" ''
-        ip rule add from ${network.vpn.subnet} lookup main priority 100
-        ip rule add to ${network.vpn.subnet} lookup main priority 100
+      server = config.me.hostname == "hikari";
+      ip = "10.2.0.2";
+      table = "200";
+      postUpFile = writeScriptFile "postUp.sh" (
+        if server then
+          # bash
+          ''
+            ip route add ${gw-ip} dev proton
+            ip rule add from ${ip} table ${table}
+            ip rule add oif %i table ${table}
+          ''
+        else
+          # bash
+          ''
+            ip rule add from ${network.vpn.subnet} lookup main priority 100
+            ip rule add to ${network.vpn.subnet} lookup main priority 100
 
-        ip rule add from ${network.lan.subnet} lookup main priority 100
-        ip rule add to ${network.lan.subnet} lookup main priority 100
-      '';
-      preDownFile = writeScriptFile "preDown.sh" ''
-        ip rule del from ${network.vpn.subnet} lookup main priority 100
-        ip rule del to ${network.vpn.subnet} lookup main priority 100
+            ip rule add from ${network.lan.subnet} lookup main priority 100
+            ip rule add to ${network.lan.subnet} lookup main priority 100
+          ''
+      );
+      preDownFile = writeScriptFile "preDown.sh" (
+        if server then
+          # bash
+          ''
+            ip route del ${gw-ip} dev proton
+            ip rule del from ${ip} table ${table}
+            ip rule del oif %i table ${table}
+          ''
+        else
+          # bash
+          ''
+            ip rule del from ${network.vpn.subnet} lookup main priority 100
+            ip rule del to ${network.vpn.subnet} lookup main priority 100
 
-        ip rule del from ${network.lan.subnet} lookup main priority 100
-        ip rule del to ${network.lan.subnet} lookup main priority 100
-      '';
+            ip rule del from ${network.lan.subnet} lookup main priority 100
+            ip rule del to ${network.lan.subnet} lookup main priority 100
+          ''
+      );
     in
     # ini
     ''
       [Interface]
-      Address = 10.2.0.2/32
-      PrivateKey = ${config.sops.placeholder."proton_vpn/pk"}
+      Address = ${ip}/32
+      PrivateKey = ${config.sops.placeholder."proton_vpn/sk"}
       ListenPort = 0
+      ${if server then "Table = ${table}" else ""}
       PostUp = ${postUpFile}
       PreDown = ${preDownFile}
 
       [Peer]
-      PublicKey = wYsaKyJteQ1gYoJZAZT0FettXDOidPhQZwl0DhaabF0=
+      PublicKey = ${config.sops.placeholder."proton_vpn/pk"}
       AllowedIPs = 0.0.0.0/0,::/0
       Endpoint = ${config.sops.placeholder."proton_vpn/endpoint"}:51820
     '';
@@ -47,5 +77,32 @@ lib.mkIf config.me.secrets.enable {
   networking.wg-quick.interfaces.proton = {
     configFile = config.sops.templates."proton-vpn.conf".path;
     autostart = false;
+  };
+
+  systemd.services."proton-portforward" = {
+    requires = [ "network-online.target" ];
+    after = [ "network-online.target" ];
+    bindsTo = [ "wg-quick-proton.service" ];
+
+    serviceConfig = {
+      ExecStartPre = pkgs.writeShellScript "get-proton-port" ''
+        OUTPUT=$(${pkgs.libnatpmp}/bin/natpmpc -g ${gw-ip} -a 1 0 tcp 60 2>/dev/null)
+        PORT=$(echo "$OUTPUT" | ${pkgs.gawk}/bin/awk '/Mapped public port/ {print $4}')
+
+        if [ -n "$PORT" ]; then
+          echo "$PORT" > /tmp/proton-vpn-port
+          echo "Successfully got port: $PORT"
+        fi
+      '';
+
+      ExecStart = pkgs.writeShellScript "renew-proton-port" ''
+        while true; do
+          ${pkgs.libnatpmp}/bin/natpmpc -g ${gw-ip} -a 1 0 tcp 60 >/dev/null 2>&1
+          ${pkgs.libnatpmp}/bin/natpmpc -g ${gw-ip} -a 1 0 udp 60 >/dev/null 2>&1
+          echo "Port forwarding renewed"
+          sleep 45
+        done
+      '';
+    };
   };
 }

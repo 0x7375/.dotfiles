d6587f72579986c1a68e20661ca7301269384c54
0x7E <codeberg.unmapped@0xaa.me>
2026-03-29 04:04:55 +0200

    Deduplicate entries nvim completion

--- nvim/lua/plugins/lsp/blink-cmp.lua
+++ nvim/lua/plugins/lsp/blink-cmp.lua
@@ -27,8 +27,14 @@ return {
       completion = { menu = { auto_show = true } },
     },
     sources = {
-      default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
+      per_filetype = {
+        lua = { inherit_defaults = true, 'lazydev' }
+      },
       providers = {
+        lsp = {
+          score_offset = 6,
+        },
         snippets = {
           score_offset = 3,
         },
@@ -41,5 +47,34 @@ return {
     },
     snippets = { preset = "luasnip" },
   },
-  opts_extend = { "sources.default" }
+  opts_extend = { "sources.default" },
+
+  -- dedup entries
+  config = function(_, opts)
+    local original = require("blink.cmp.completion.list").show
+    ---@diagnostic disable-next-line: duplicate-set-field
+    require("blink.cmp.completion.list").show = function(ctx, items_by_source)
+      local seen = {}
+      local priority = { "lsp", "snippets", "path", "buffer", "lazydev" }
+
+      if items_by_source.lsp then
+        table.sort(items_by_source.lsp, function(a, b)
+          if a.kind == 15 and b.kind ~= 15 then return true end
+          return false
+        end)
+      end
+
+      for _, id in ipairs(priority) do
+        if items_by_source[id] then
+          items_by_source[id] = vim.iter(items_by_source[id]):filter(function(item)
+            if seen[item.label] then return false end
+            seen[item.label] = true
+            return true
+          end):totable()
+        end
+      end
+      return original(ctx, items_by_source)
+    end
+    require("blink.cmp").setup(opts)
+  end,
 }

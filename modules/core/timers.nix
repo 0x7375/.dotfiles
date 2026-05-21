79a2e2eb3643425e9dd2406a09f2714227c3b512
0x7E <codeberg.unmapped@0xaa.me>
2026-05-22 01:42:33 +0200

    Make auto update timer commit the change

--- modules/core/timers.nix
+++ modules/core/timers.nix
@@ -17,7 +17,23 @@
 
       systemd.services.auto-input = {
         script = ''
-          ${lib.getExe pkgs.lix} flake update auto-update nur zen-browser --flake ${config.me.flakeDir}
+          pushd "${config.me.flakeDir}"
+          git stash
+
+          nix flake update auto-update nur zen-browser --flake .
+          git add flake.lock
+          if ! git diff --cached --quiet; then
+            last_msg=$(git log -1 --pretty=%s)
+            if [[ "$last_msg" == "chore: flake.lock update" ]]; then
+              git -c commit.gpgsign=false commit --amend --no-edit
+            else
+              git -c commit.gpgsign=false commit -m "chore: flake.lock update"
+            fi
+          fi
+
+          git stash pop
+          popd
+
           [[ -d "$HOME/repos/nixpkgs" ]] && git -C "$HOME/repos/nixpkgs" pull
           [[ -d "$HOME/repos/home-manager" ]] && git -C "$HOME/repos/home-manager" pull
           [[ -d "$HOME/repos/nix-darwin" ]] && git -C "$HOME/repos/nix-darwin" pull
@@ -27,7 +43,11 @@
           Type = "oneshot";
           User = config.me.user;
         };
-        path = [ pkgs.git ];
+        path = with pkgs; [
+          lix
+          git
+          openssh
+        ];
       };
 
       systemd.timers.clean-old-trash = {

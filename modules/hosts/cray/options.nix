0c82023c8b41178735cad1945cff0bb5fb3f2f7a
0x7E <codeberg.unmapped@0xaa.me>
2026-06-15 21:26:27 +0200

    Oops disabled secure boot

--- modules/hosts/cray/options.nix
+++ modules/hosts/cray/options.nix
@@ -2,7 +2,10 @@
   flake.modules.nixos.cray = {
     me = {
       tpm.enable = true;
-      boot.encryption.enable = true;
+      boot = {
+        encryption.enable = true;
+        secureBoot.enable = true;
+      };
       desktop.refreshRate = 240;
     };
   };
--- modules/hosts/naitoh/options.nix
+++ modules/hosts/naitoh/options.nix
@@ -2,7 +2,10 @@
   flake.modules.nixos.naitoh = {
     me = {
       tpm.enable = true;
-      boot.encryption.enable = true;
+      boot = {
+        encryption.enable = true;
+        secureBoot.enable = true;
+      };
       desktop = {
         scaling = 1.2;
         barFontSize = 11;

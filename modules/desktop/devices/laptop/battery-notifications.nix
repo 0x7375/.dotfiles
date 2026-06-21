941c2ace04ab9b2e9c9aee98e3438e44d00a4b92
0x7E <codeberg.unmapped@0xaa.me>
2026-06-21 19:33:13 +0200

    Merge and simplify battery stuff

--- modules/desktop/devices/laptop/battery-notifications.nix
+++ modules/desktop/devices/laptop/battery-notifications.nix
@@ -1,13 +1,12 @@
 {
   flake.modules.nixos.laptop =
     {
-      lib,
       pkgs,
       config,
       ...
     }:
     {
-      systemd.user.timers.battery-timer = {
+      systemd.user.timers.battery-check = {
         wantedBy = [ "timers.target" ];
         timerConfig = {
           OnBootSec = "1m";
@@ -15,54 +14,13 @@
         };
       };
 
-      systemd.user.services.battery-timer = {
-        script = ''
-          ${lib.getExe' pkgs.systemd "systemctl"} start --user battery-notify
-          ${lib.getExe' pkgs.systemd "systemctl"} start --user battery-check
-        '';
-        serviceConfig.Type = "oneshot";
-        wantedBy = [ "multi-user.target" ];
-      };
-
-      systemd.user.services.battery-notify = {
+      systemd.user.services.battery-check = {
         path = with pkgs; [
           gnugrep
           acpi
           libnotify
           systemd
-        ];
-        script = ''
-          warning_level=20
-          full_level=90
-
-          empty_file=/tmp/batteryempty
-          full_file=/tmp/batteryfull
-
-          battery_discharging=$(acpi -b | grep -c "Discharging")
-          battery_level=$(acpi -b | grep -E "remaining|charged|zero" | grep -P -o '[0-9]+(?=%)' || echo 0)
-
-          if [[ $battery_discharging -eq 1 ]] && [[ -f $full_file ]]; then
-              rm $full_file
-          elif [[ $battery_discharging -eq 0 ]] && [[ -f $empty_file ]]; then
-              rm $empty_file
-          fi
-
-          if [[ $battery_level -ge $full_level && $battery_discharging -eq 0 && ! -f $full_file ]]; then
-              ${lib.getExe pkgs.my.notify} "Battery Charged" "Battery is fully charged." -i "battery-4"
-              touch $full_file
-          elif [[ $battery_level -le $warning_level ]] && [[ $battery_discharging -eq 1 ]] && [[ ! -f $empty_file ]]; then
-              ${lib.getExe pkgs.my.notify} "Low battery" "$battery_level% of battery remaining." -i battery-1 -u critical
-              touch $empty_file
-          fi
-        '';
-        serviceConfig.Type = "oneshot";
-      };
-
-      systemd.user.services."battery-check" = {
-        path = with pkgs; [
-          gnugrep
-          acpi
-          systemd
+          my.notify
         ];
         script =
           let
@@ -71,19 +29,32 @@
           # bash
           ''
             critical_level=7
+            full_level=90
 
-            is_discharging() {
-              acpi -b | grep -q "Discharging"
-            }
+            critical_file=/tmp/batterycritical
+            full_file=/tmp/batteryfull
+
+            is_discharging() { acpi -b | grep -q "Discharging"; }
 
             battery_level=$(acpi -b | grep -E "remaining|charged|zero" | grep -P -o '[0-9]+(?=%)' || echo 0)
 
+            is_discharging && rm -f $full_file
+
+            if [[ $battery_level -ge $full_level && ! -f $full_file ]] && ! is_discharging; then
+                notify "Battery Charged" "Battery is fully charged." -i "battery-4"
+                touch $full_file
+            fi
+
             if [[ $battery_level -le $critical_level ]] && is_discharging; then
-              ${lib.getExe pkgs.my.notify} "Very low battery" "System will ${action} in 120 seconds!" -i battery -u "error"
+              notify "Very low battery" "System will ${action} in 120 seconds!" -i battery -u critical -t 120000
 
               sleep 120
 
-              is_discharging && systemctl ${action}
+              if is_discharging; then
+                systemctl ${action}
+              else
+                rm $critical_file
+              fi
             fi
           '';
         serviceConfig.Type = "oneshot";

760b754a5ddc72b9ae129cb95abda03aa4cecefb
0x7E <codeberg.unmapped@0xaa.me>
2026-06-22 04:47:42 +0200

    nvim: Set my own functions as globals for lua_ls diagnostics

--- nvim/after/lsp/lua_ls.lua
+++ nvim/after/lsp/lua_ls.lua
@@ -3,6 +3,7 @@ return {
   ---@type lspconfig.settings.lua_ls
   settings = {
     Lua = {
+      diagnostics = { globals = { "map", "del", "pack", "on_event", "on_filetype" } },
       format = { enable = false }, -- using stylua
       hint = {
         enable = true,

a13e72435a2201ed0dfd2e5074b567295327841d
0x7E <codeberg.unmapped@0xaa.me>
2026-06-22 02:13:04 +0200

    Disable chore flake update for now

--- modules/core/timers.nix
+++ modules/core/timers.nix
@@ -17,22 +17,22 @@
 
       systemd.services.auto-input = {
         script = ''
-          pushd "${config.me.flakeDir}"
-          git stash
-
-          nix flake update auto-update nur zen-browser --flake .
-          git add flake.lock
-          if ! git diff --cached --quiet; then
-            last_msg=$(git log -1 --pretty=%s)
-            if [[ "$last_msg" == "chore: flake.lock update" ]]; then
-              git -c commit.gpgsign=false commit --amend --no-edit
-            else
-              git -c commit.gpgsign=false commit -m "chore: flake.lock update"
-            fi
-          fi
-
-          git stash pop
-          popd
+          # pushd "${config.me.flakeDir}"
+          # git stash
+          #
+          # nix flake update auto-update nur zen-browser --flake .
+          # git add flake.lock
+          # if ! git diff --cached --quiet; then
+          #   last_msg=$(git log -1 --pretty=%s)
+          #   if [[ "$last_msg" == "chore: flake.lock update" ]]; then
+          #     git -c commit.gpgsign=false commit --amend --no-edit
+          #   else
+          #     git -c commit.gpgsign=false commit -m "chore: flake.lock update"
+          #   fi
+          # fi
+          #
+          # git stash pop
+          # popd
 
           [[ -d "$HOME/repos/nixpkgs" ]] && git -C "$HOME/repos/nixpkgs" pull
           [[ -d "$HOME/repos/home-manager" ]] && git -C "$HOME/repos/home-manager" pull

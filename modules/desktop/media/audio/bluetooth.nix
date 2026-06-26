6f6edb7805038e571366ce126e04fd46776b007f
0x7E <codeberg.unmapped@0xaa.me>
2026-06-26 10:15:10 +0200

    bluetooth: Remove useless conf entry

--- modules/desktop/media/audio/bluetooth.nix
+++ modules/desktop/media/audio/bluetooth.nix
@@ -16,9 +16,6 @@
             MultiProfile = "multiple";
             Privacy = "device";
             FastConnectable = true;
-            Enable = "Control,Gateway,Headset,Media,Sink,Socket,Source";
-            # Uncomment on first connection airpods
-            # ControllerMode = "bredr";
           };
         };
       };

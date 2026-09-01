5036ffbd1240ba24b37a27b866fe598d3ec7e142
0x7E <codeberg.unmapped@0xaa.me>
2026-09-01 06:21:35 +0200

    yubikey-touch-detector: only clear the sent notification

--- modules/desktop/security/notify.nix
+++ modules/desktop/security/notify.nix
@@ -1,5 +1,3 @@
-{ self, ... }:
-
 {
   flake.modules.nixos.desktop =
     {
@@ -11,7 +9,10 @@
       systemd.packages = [ pkgs.yubikey-touch-detector ];
 
       systemd.user.services.yubikey-touch-detector = {
-        path = [ pkgs.gnupg ];
+        path = with pkgs; [
+          gnupg
+          glib
+        ];
         environment = {
           YUBIKEY_TOUCH_DETECTOR_LIBNOTIFY = "false";
           YUBIKEY_TOUCH_DETECTOR_NOSOCKET = "false";
@@ -22,10 +23,14 @@
             if [[ "$msg" == "U2F_1" ]]; then
               active_panel=$(${lib.getExe pkgs.noctalia} msg status | ${lib.getExe pkgs.jq} -r .activePanelId)
               [[ $active_panel == "polkit" ]] && continue
-
-              ${lib.getExe pkgs.my.notify} "Security key" "Touch required" -i "fingerprint" -t 0
-            else
-              ${lib.getExe pkgs.noctalia} msg notification-clear-active
+              NOTIF_ID=$(${lib.getExe pkgs.my.notify} "Security key" "Touch required" -i "fingerprint" -t 0 -p)
+            elif [[ -n "$NOTIF_ID" ]]; then
+              gdbus call --session \
+                --dest org.freedesktop.Notifications \
+                --object-path /org/freedesktop/Notifications \
+                --method org.freedesktop.Notifications.CloseNotification \
+                "$NOTIF_ID" >/dev/null 2>&1
+              NOTIF_ID=""
             fi
           done) &
         '';
